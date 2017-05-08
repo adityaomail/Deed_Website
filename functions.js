d1b7613da6ba1a624c4c80d718a9f74e7ec7f3b1
Aditya Tripathi <adityaomail@gmail.com>
2017-05-08 15:46:41 +0530

    Add files via upload
    Firebase authentication without federation

--- functions.js
+++ functions.js
@@ -1,7 +1,36 @@
+var databaseRef = firebase.database();
+
 function alertMe()
 {
   var data = document.getElementById('usr_data');
 
-  var databaseRef = firebase.database();
+
   databaseRef.ref("Aditya").set("This is my name");
+}
+
+function registerUser(){
+  var firebaseAuth = firebase.auth();
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var errcode;
+  var errMessage;
+  if(firebaseAuth.createUserWithEmailAndPassword(email, password).catch(function(error){
+       errcode = error.code;
+       errMessage = error.meassage;
+  })){alert("Successfull");
+
+  //if successfull then regsiter all the user details in database
+
+  databaseRef.ref()
+
+
+}
+  else {
+    alert(errMessage);
+  }
+
+
+
+
+
 }
